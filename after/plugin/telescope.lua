674f3d93931e5f5242bad510963215f10b5c4cd0
akash-kola-test <akash.kola.test@gmail.com>
2025-06-21 15:43:16 +0530

    angular, lsp, and nvim common setup

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,4 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live Grep' })
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,7 @@
+return {
+	{'neovim/nvim-lspconfig', tag = 'v1.8.0', pin = true},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'williamboman/mason.nvim'},
+	{'williamboman/mason-lspconfig.nvim'},
+}
